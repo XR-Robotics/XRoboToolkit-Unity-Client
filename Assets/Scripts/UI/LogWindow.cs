3b3da785ae2c822c3af7abd4a11adc8b0117ca71
Liuchuan Yu <liuchuan.yu@bytedance.com>
2025-07-24 16:50:04 -0700

    Add LogWindow

---
+++ Assets/Scripts/UI/LogWindow.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogWindow : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private static LogWindow _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private static void Message(string message)
+    {
+        if (_instance != null)
+        {
+            // add time prefix of local timezone to the message
+            string timePrefix = $"[{System.DateTime.Now:HH:mm:ss}] ";
+            _instance.text.text += $"{timePrefix}{message}\n";
+        }
+    }
+
+    public static void Info(string info)
+    {
+        // white color text
+        Message($"<color=white>{info}</color>");
+    }
+
+    public static void Warn(string info)
+    {
+        // yellow color text
+        Message($"<color=yellow>{info}</color>");
+    }
+
+    public static void Error(string info)
+    {
+        // red color text
+        Message($"<color=red>{info}</color>");
+    }
+}
